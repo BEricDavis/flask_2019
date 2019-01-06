ab8bad1d4c7706a57e0e78af76e38d422c771b0d
Eric Davis <brian.davis@elsevier.com>
2019-01-06 15:39:27 -0500

    02.02 - Add index.html template and render it instead of the static html

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Eric'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>    
-    </html>
-    
-    '''
+    return render_template('index.html', title='Home', user=user)
