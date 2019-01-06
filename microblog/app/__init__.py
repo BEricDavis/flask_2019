9b2043e7d59ffcc1aae730ee7580c69b307194cb
Eric Davis <brian.davis@elsevier.com>
2019-01-06 16:21:40 -0500

    03.01 import flask-wtf; create config.py and import in __init__

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+# the import of Config works though pycharm complains it can't resolve it
+from config import Config
 
 # "app" here is an instance of the Flask class.
 app = Flask(__name__)
+app.config.from_object(Config)
 
-# "app" here is the app packags
+# "app" here is the app packags; thiis works though PyCharm complains
 from app import routes
