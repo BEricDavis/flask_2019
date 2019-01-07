6641a37e0d3219f974cc222b25db207dd88ae696
Eric Davis <brian.davis@elsevier.com>
2019-01-06 20:23:08 -0500

    04.01 - import flask-sqlalchemy and flask-migrate; add configs and instantiation

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 # the import of Config works though pycharm complains it can't resolve it
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # "app" here is an instance of the Flask class.
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # "app" here is the app packags; thiis works though PyCharm complains
-from app import routes
+from app import routes, models
