4fd76216f4050f07f0d639969b88d252d1f87715
Eric Davis <brian.davis@elsevier.com>
2019-01-09 20:16:33 -0500

    05.02 - configure flask-login

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+# Extensions get added to this file
 # the import of Config works though pycharm complains it can't resolve it
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
 
 # "app" here is an instance of the Flask class.
@@ -10,6 +12,6 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
 # "app" here is the app packags; thiis works though PyCharm complains
 from app import routes, models
