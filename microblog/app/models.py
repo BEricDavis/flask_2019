963b9cc83d891a4e5e3b0ccf03c29f9a6668b1c2
Eric Davis <brian.davis@elsevier.com>
2019-01-10 19:21:42 -0500

    06.02 - add avatar to profile page

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from app import db, login
 from flask_login import UserMixin # provides the required properties and methods for flask_login
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
@@ -18,6 +19,10 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return  'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format( digest, size )
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
